931532b8498b421aeb246d1cd3b3718bec920446
Nikhil Bolar <nikhilbolar@nikhils-macbook-pro.local>
2019-02-03 15:02:36 +0300

    Initial Commit

---
+++ Link It/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Link It
+//
+//  Created by Nikhil Bolar on 2/3/19.
+//  Copyright © 2019 Nikhil Bolar. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
